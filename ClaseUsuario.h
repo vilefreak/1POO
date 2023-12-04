053218e6f77f66dba294be7dbfb5e9bc080ca2a4
Mauricio Villalobos Gutiérrez <145585413+vilefreak@users.noreply.github.com>
2023-12-03 18:16:13 -0600

    Former ClaseEmpleado V2

---
+++ ClaseUsuario.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Empleado {
+private:
+    string username;
+    string password;
+    int loginIntentos;
+
+public:
+    Empleado(string username, string password) : username(username), password(password), loginIntentos(0) {}
+
+    bool login(string intentoUsername, string intentoContrasena) {
+        if (intentoUsername == username && intentoContrasena == password) {
+            loginIntentos = 0;
+            return true;
+        } else {
+            loginIntentos++;
+            return false;
+        }
+    }
+
+    bool isLocked() {
+        if (username == "admin" && loginIntentos >= 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void eliminarAdmin() {
+        username = "";
+        password = "";
+    }
+};

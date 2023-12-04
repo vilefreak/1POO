801bdff05e690c8bbaf9eea3f7ebcfb26cc7698b
Mauricio Villalobos Gutiérrez <145585413+vilefreak@users.noreply.github.com>
2023-12-03 18:15:01 -0600

    ClaseEmpleado rework

--- ClaseEmpleado.h
+++ ClaseEmpleado.h
@@ -1,38 +1,17 @@
-//include <F:\UNI TEC\Primer Semestre\Programación Objetos\Proyecto Empleados\ClaseEmpleado.h>
-
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
 using namespace std;
 
 class Empleado {
-private:
-    string username;
-    string password;
-    int loginIntentos;
-
 public:
-    Empleado(string username, string password) : username(username), password(password), loginIntentos(0) {}
+    int id;
+    string name;
+    double sueldo;
 
-    bool login(string intentoUsername, string intentoContrasena) {
-        if (intentoUsername == username && intentoContrasena == password) {
-            loginIntentos = 0;
-            return true;
-        } else {
-            loginIntentos++;
-            return false;
-        }
-    }
-
-    bool isLocked() {
-        if (username == "admin" && loginIntentos >= 3) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    void eliminarAdmin() {
-        username = "";
-        password = "";
-    }
+    Empleado() : id(0), name(""), sueldo(1000) {}  // default constructor
+    Empleado(int id, string name) : id(id), name(name), sueldo(1000) {}
 };
